f4547b250e9aec923464b0c506fedbc82f8fa681
abusalam <abusalamparvezalam@gmail.com>
2013-05-26 19:12:09 +0530

    Submodules Added

---
+++ MySQLServer.php
@@ -0,0 +1,10 @@
+<?php 
+define("HOST_Name","localhost");
+define("MySQL_User","root");
+define("MySQL_Pass","mysql");
+define("MySQL_DB","CHSEIS");
+define("MySQL_Pre","CHSEIS_");
+define("LifeTime","20");
+define("BaseDIR","/chseis/"); // path to the php scripts on the webserver for the menus
+define("AppTitle","CHSEIS - Child Health Screening and Early Intervention Services - Paschim Medinipur");
+?>

548ad66fa4a7839bcc8b169eec6f230378354ac8
Abu Salam <abu@HP-CQ3550IX>
2013-06-06 18:38:17 +0530

    Added AppForm

---
+++ AppForm.php
@@ -0,0 +1,121 @@
+<?php 
+use LibDB as FN;
+require_once('functions.php');
+FN\session_auth();
+FN\HtmlHeader("Data Entry");
+FN\IncludeCSS();
+FN\jQueryInclude();
+?>
+<script type="text/javascript">
+var weekday=new Array();
+weekday[0]="Sunday";
+weekday[1]="Monday";
+weekday[2]="Tuesday";
+weekday[3]="Wednesday";
+weekday[4]="Thursday";
+weekday[5]="Friday";
+weekday[6]="Saturday";
+var current_id=1;
+$(function() {
+	$( ".datepick" ).datepicker({minDate: "1D", 
+								maxDate: "2M",
+								dateFormat: 'dd/mm/yy',
+								showOtherMonths: true,
+								selectOtherMonths: true,
+								showButtonPanel: true,
+								changeMonth: true,
+							  changeYear: true,
+								showAnim: "slideDown",
+								onClose:function(VisitDate,DateCtrl){
+										WeekDay=new Date();
+										$("#SchWeekday").text(weekday[$(this).datepicker('getDate').getDay()]);}
+								});
+	$("#EnrMale").blur(function(){
+								$("#EnrTotal").text( Number(this.value)+Number($("#EnrFem").val()));});
+	$("#EnrFem").blur(function(){
+								$("#EnrTotal").text( Number(this.value)+Number($("#EnrMale").val()));});
+	$("#AddRow").click(function(){
+				var NewRow;
+				NewRow=$("#Row").clone(true);
+				NewRow.attr("id","Row"+current_id);
+				NewRow.find("#EnrFem").attr("id","EnrFem"+current_id);
+				current_id=current_id+1;
+				NewRow.appendTo(".DGrid");
+	});
+	});
+</script>
+<style>
+	.TxtInput{
+		border: 2px solid darkgray;
+		width: 98%;
+		padding: 0px;
+		margin: 0px;
+	}
+	.TxtInput:hover{
+		border: 2px solid #fbd850;
+	}
+	.TxtInput:focus{
+		border: 2px solid #1c94c4;
+	}
+	th{
+		text-align: center;
+	}
+</style>
+</head>
+<body>
+	<div class="TopPanel">
+		<div class="LeftPanelSide"></div>
+		<div class="RightPanelSide"></div>
+		<h1>
+			<?php echo AppTitle;?>
+		</h1>
+	</div>
+	<div class="Header"></div>
+	<?php 
+	require_once("topmenu.php");
+	?>
+	<div class="content">
+		<div style="background-color: darkgray;margin-top: 10px;">
+			<h1>Annexure-A</h1>
+			<h2 style="text-align: center">Action Plan Format (Micro Planning)</h2>
+			<div style="border: 2px solid black;">
+				<h3 style="text-align: center;border-bottom: 2px solid black;background-color: #333333;color: white;">National Rural Health Mission - Child Health Screening and Early Intervention Services</h3>
+				<table rules="all" width="100%" class="DGrid">
+					<tr>
+						<th rowspan="2">Sr. No.</th>
+						<th rowspan="2">Name of Institution</th>
+						<th colspan="3">Number of Children in Institution</th>
+						<th rowspan="2">School Contact (Mobile) No.</th>
+						<th rowspan="2">Visit Date</th>
+						<th rowspan="2">Day</th>
+					</tr>
+					<tr>
+						<th>Male</th>
+						<th>Female</th>
+						<th>Total</th>
+					</tr>
+					<tr id="Row">
+						<td><?php echo '1';?></td>
+						<td><input id="InstName" class="TxtInput" name="InstName[]" type="text" /></td>
+						<td><input id="EnrMale" class="TxtInput" name="EnrMale[]" type="text" /></td>
+						<td><input id="EnrFem" class="TxtInput" name="EnrFem[]" type="text" /></td>
+						<td><span id="EnrTotal">0</span></td>
+						<td><input id="Mobile" maxlength="10" class="TxtInput" name="Mobile[]" type="text" /></td>
+						<td><input id="VisitDate" class="TxtInput datepick" name="VisitDate[]" type="text" /></td>
+						<td><span id="SchWeekday"></span></td>
+					</tr>
+				</table>
+				<input type="button" id="AddRow" value="Add New" />
+				<p>Content goes here</p>
+			</div>
+		</div>		
+	</div>
+	<div class="pageinfo">
+		<?php FN\pageinfo(); ?>
+	</div>
+	<div class="footer">
+		<?php FN\footerinfo();?>
+	</div>
+</body>
+</html>
+
